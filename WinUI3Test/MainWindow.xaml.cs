5de3be3a38ec237fea2242b77cb1b1d58f624f5f
Eduardo Braga <braga.eduardoTL@outlook.com>
2022-11-23 11:54:21 -0300

    Add project files.

---
+++ WinUI3Test/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Input;
+using System.Collections.ObjectModel;
+
+namespace WinUI3Test;
+
+public sealed partial class MainWindow : Window
+{
+    public ObservableCollection<string> oi = new();
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        oi.Add("whatever");
+    }
+
+    private void showbutton_Click(object sender, RoutedEventArgs e)
+    {
+        if(addtolist is null)
+        {
+            navi.FindName("addtolist");
+        }
+        else
+        {
+            UnloadObject(addtolist);
+        }
+    }
+}
